c33c87592c5d5ded4b3399950b60eb0cc276b964
Chidubem Ogbudibe <ogbudibe@mail.utoronto.ca>
2019-10-18 05:47:05 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,9 @@
 def turn_count(board)
   moves = 0;
-  board.each do |place| {if(place == "X" || place == "O") moves += 1 end}
+  board.each do |place|
+  if(place == "X" || place == "O"
+    moves += 1 
+  end
   return moves
 end
 
